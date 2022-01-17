737faa2b26a63332bdbb9fffba46e5b6cfcaef9a
yubo <yubo@mini1.cn>
2022-01-17 17:41:40 +0800

    proto demo

--- protobuf/main.cpp
+++ protobuf/main.cpp
@@ -1,12 +1,46 @@
-#include <iostream>                  // for std::cout
-#include <utility>                   // for std::pair
-#include <algorithm>                 // for std::for_each
-#include <boost/graph/graph_traits.hpp>
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/dijkstra_shortest_paths.hpp>
-
-using namespace boost;
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include "src/generated/cpp/SearchRequest.pb.h"
+#include "src/generated/cpp/MyMessage.pb.h"
 
 int main(int, char *[])
 {
+    std::string query("hello");
+
+    SearchRequest *sr = new SearchRequest();
+    sr->set_corpus(SearchRequest_Corpus_UNIVERSAL);
+    sr->set_query(query);
+    sr->set_page_number(2);
+    sr->set_result_per_page(1);
+    sr->set_i(MyMessage1_EnumAllowingAlias_UNKNOWN);
+
+    std::string s = "中文";
+    std::cout << s << std::endl;
+
+
+    std::ofstream ofs("/tmp/sensor.data", std::ios_base::out | std::ios_base::binary);
+    sr->SerializeToOstream(&ofs);
+
+    std::string bytes = sr->SerializeAsString();
+    const char *array = bytes.data();
+    int size = bytes.size();
+
+    for (int i = 0; i < size; ++i)
+    {
+        std::cout << std::setw(5) << std::setfill(' ') << array[i];
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < size; ++i)
+    {
+        std::cout << std::setw(5) << std::hex << std::setfill(' ')  << (int) array[i];
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < size; ++i)
+    {
+        std::cout << std::setw(5) << std::dec << std::setfill(' ')  << (int) array[i];
+    }
+    std::cout << std::endl;
+    ofs.close();
+
 }
